69245f14f8a7abd7b4b0266979c40d36af66214c
Renan Miranda <132950877+renanmiranda99@users.noreply.github.com>
2023-10-28 17:02:23 -0300

    Add files via upload

---
+++ financialy-page/ContaUniversitaria.js
@@ -0,0 +1,16 @@
+import { ContaBancaria } from "./ContaBancaria.js";
+
+export class ContaUniversitaria extends ContaBancaria{
+    constructor(numero,agencia,saldo){
+        super(numero,agencia,"Conta Universitaria",saldo)
+    }
+
+    sacar(valor){
+        if(valor < 500){
+            this.saldo -= valor;
+            console.log(`Saque de R$ ${valor} realizado`);
+        }else{
+            console.log("Valor invalido!")
+        };
+    };
+};
---
+++ financialy-page/script.js
@@ -0,0 +1,79 @@
+import { ContaCorrente } from "./ContaCorrente.js";
+import { ContaPoupanca } from "./ContaPoupanca.js";
+import { ContaUniversitaria } from "./ContaUniversitaria.js";
+
+//array p armazenas as contas
+const contas = [];
+
+export function inserirConta() {
+
+    const numero = document.getElementById("numero").value;
+    const agencia = document.getElementById("agencia").value;
+    const tipo = document.getElementById("tipo").value;
+    const saldo = parseFloat(document.getElementById("saldo").value);
+
+    //verificando se todos os campos foram preenchidos
+    if (!numero || !agencia || !tipo || isNaN(saldo)) {
+        alert("Preencha todos os campos corretamente!");
+        return;
+    }
+
+
+    //criando objeto novaConta
+    let novaConta;
+
+    switch (tipo) {
+        case "Conta Corrente":
+            novaConta = new ContaCorrente(numero, agencia, saldo, false);
+            break;
+        case "Conta Poupanca":
+            novaConta = new ContaPoupanca(numero, agencia, saldo);
+            break;
+        case "Conta Universitária":
+            novaConta = new ContaUniversitaria(numero, agencia, saldo);
+            break;
+    }
+
+    //adicionando ao vetor contas
+    contas.push(novaConta);
+
+    //limpando os campos
+    document.getElementById("numero").value = "";
+    document.getElementById("agencia").value = "";
+    document.getElementById("tipo").value = "Conta Corrente";
+    document.getElementById("saldo").value = "";
+
+    alert("Conta inserida com sucesso!");
+}
+
+export function visualizarContas() {
+    const listaContas = document.getElementById("listacontas");
+    //limpando o logger das contas
+    listaContas.innerHTML = "";
+    //percorrendo o vetor contas[]
+    for (const conta of contas) {
+        //criando um elemento <li>
+        const itemLista = document.createElement("li");
+        itemLista.textContent = `Número: ${conta.numero}, Agência: ${conta.agencia}, Tipo: ${conta.tipo}, Saldo: R$ ${conta.saldo.toFixed(2)}`;
+        listaContas.appendChild(itemLista);
+    }
+
+}
+
+export function deletarContas(){
+
+    const numeroConta = document.getElementById("contaDeleta").value;
+    const indice = contas.findIndex(conta => conta.numero === numeroConta);
+
+    if (indice !== -1) {
+        contas.splice(indice, 1); // Remove a conta no índice encontrado
+        alert("Conta deletada com sucesso!");
+        visualizarContas();
+    } else {
+        alert("Conta não encontrada.");
+    }
+
+    document.getElementById("contaDeleta").value="";
+}
+
+
